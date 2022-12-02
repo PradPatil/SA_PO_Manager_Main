2b5663eb4c9d741b3774ac8f392afd7e4ca861ee
PradPatil <pradnya.patil2@capita.com>
2022-12-02 15:45:51 +0530

    Second commit

---
+++ src/test/java/Capita/PO_Manager_Main/newfile.java
@@ -0,0 +1,10 @@
+package Capita.PO_Manager_Main;
+
+public class newfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My new file added");
+	}
+
+}
