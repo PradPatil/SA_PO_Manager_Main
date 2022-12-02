2260d9a4c6ec863fbf592d46b72c04398498467f
PradPatil <pradnya.patil2@capita.com>
2022-12-02 15:59:07 +0530

    Third commit

--- src/test/java/Capita/PO_Manager_Main/newfile.java
+++ src/test/java/Capita/PO_Manager_Main/newfile.java
@@ -5,6 +5,7 @@ public class newfile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My new file added");
+System.out.println("This is the cloned proj changes");
 	}
 
 }
